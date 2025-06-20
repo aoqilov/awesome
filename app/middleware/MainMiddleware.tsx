76cf077e374a93b4a646d395abe76669e68e5b1c
abdulaziz <aoqilov88@gmail.com>
2025-06-20 21:39:07 +0500

    initial commit

--- app/middleware/MainMiddleware.tsx
+++ app/middleware/MainMiddleware.tsx
@@ -9,43 +9,43 @@ const MainMiddleware = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const { chat_id } = useQueryParam();
-  const [, setUser] = useState<undefined | RecordAuthResponse<RecordModel>>();
+  const [setUser] = useState<undefined | RecordAuthResponse<RecordModel>>();
 
-  useEffect(() => {
-    const authenticateUser = async () => {
-      try {
-        const data = await pb
-          .collection("users")
-          .authWithPassword(`${chat_id}@gmail.com`, chat_id);
+  // useEffect(() => {
+  //   const authenticateUser = async () => {
+  //     try {
+  //       const data = await pb
+  //         .collection("users")
+  //         .authWithPassword(`${chat_id}@gmail.com`, chat_id);
 
-        setUser(data);
+  //       setUser(data);
 
-        if (!pathname.includes("dashboard")) {
-          navigate(`/dashboard/home?chat_id=${chat_id}`, { replace: true });
-        }
-      } catch (error) {
-        pb.authStore.clear();
-        console.error("Authentication failed:", error);
-        navigate(`/register?chat_id=${chat_id}`, { replace: true });
-      }
-    };
+  //       if (!pathname.includes("dashboard")) {
+  //         navigate(`/dashboard/home?chat_id=${chat_id}`, { replace: true });
+  //       }
+  //     } catch (error) {
+  //       pb.authStore.clear();
+  //       console.error("Authentication failed:", error);
+  //       navigate(`/register?chat_id=${chat_id}`, { replace: true });
+  //     }
+  //   };
 
-    if (!chat_id) {
-      pb.authStore.clear();
-      navigate(`/register?chat_id=${chat_id}`, { replace: true });
-      return;
-    }
+  //   if (!chat_id) {
+  //     pb.authStore.clear();
+  //     navigate(`/register?chat_id=${chat_id}`, { replace: true });
+  //     return;
+  //   }
 
-    // const alreadyLoggedIn =
-    //   pb.authStore.isValid &&
-    //   pb.authStore.record?.email === `${chat_id}@gmail.com`;
+  //   // const alreadyLoggedIn =
+  //   //   pb.authStore.isValid &&
+  //   //   pb.authStore.record?.email === `${chat_id}@gmail.com`;
 
-    // console.log(alreadyLoggedIn);
+  //   // console.log(alreadyLoggedIn);
 
-    // if (!alreadyLoggedIn) {
-    authenticateUser();
-    // }
-  }, [chat_id, pathname, navigate]);
+  //   // if (!alreadyLoggedIn) {
+  //   authenticateUser();
+  //   // }
+  // }, [chat_id, pathname, navigate]);
 
   return (
     <div className="cw-screen h-screen w-full">
